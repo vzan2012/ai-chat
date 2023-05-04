3afdb8c9123e5a2fa6ee24e805b4a5e9f3be0841
Deepak Guptha <vzan2012@gmail.com>
2023-05-04 17:18:54 +0530

    feat: updated stop button functionality and stream data

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
 import "./style.css";
 
-console.log("Testing in JS");
-console.log(import.meta.env);
-console.log(import.meta.env);
-
 // Getting values from env variables
 const API_KEY = import.meta.env.VITE_API_KEY;
 const API_URL = import.meta.env.VITE_API_URL;
@@ -14,8 +10,18 @@ const resultText = document.getElementById("resultText");
 const generateBtn = document.getElementById("generateBtn");
 const stopBtn = document.getElementById("stopBtn");
 
+const decoder = new TextDecoder("utf-8");
+
+let controller = null;
+
 // Fetch and Generate the response from the API
 const generate = async () => {
+  stopBtn.disabled = false;
+
+  // For Aborting the requests
+  controller = new AbortController();
+  const signal = controller.signal;
+
   // Checking validation
   if (!promptInput.value) {
     alert("Please enter a prompt");
@@ -37,16 +43,38 @@ const generate = async () => {
         model: "gpt-3.5-turbo",
         messages: [{ role: "user", content: promptInput.value }],
         temperature: 0.7,
+        stream: true,
       }),
+      signal,
     });
 
-    const responseData = await response.json();
-    resultText.innerText = responseData.choices[0].message.content;
+    const reader = response.body.getReader();
+
+    resultText.innerText = "";
+
+    while (true) {
+      const { value, done } = await reader.read();
+
+      if (done) break;
+
+      // Parse the Response Data
+      parseReponseData(value);
+    }
+
+    // Remove this code when stream is enabled
+    // const responseData = await response.json();
+    // resultText.innerText = responseData.choices[0].message.content;
   } catch (error) {
-    resultText.innerText = "Error occured while generating ...";
-    console.error("Error: " + error);
+    if (signal.aborted) {
+      resultText.innerText = "Request aborted";
+    } else {
+      resultText.innerText = "Error occured while generating ...";
+      console.error("Error: ", error);
+    }
   } finally {
     generateBtn.disabled = false;
+    stopBtn.disabled = true;
+    controller = null;
   }
 };
 
@@ -56,3 +84,28 @@ generateBtn.addEventListener("click", generate);
 promptInput.addEventListener("keyup", (e) => {
   if (e.key === "Enter") generate();
 });
+
+stopBtn.addEventListener("click", () => {
+  if (controller) {
+    controller.abort();
+    controller = null;
+  }
+});
+
+// Parse Response Data
+const parseReponseData = (value) => {
+  const decoderChunk = decoder.decode(value);
+  const lines = decoderChunk.split("\n");
+  const parsedLines = lines
+    .map((line) => line.replace(/^data:/, "").trim())
+    .filter((line) => line !== "" && line !== "[DONE]")
+    .map((line) => JSON.parse(line));
+
+  for (const parseLine of parsedLines) {
+    const { choices } = parseLine;
+    const { delta } = choices[0];
+    const { content } = delta;
+
+    if (content) resultText.innerText += content;
+  }
+};
